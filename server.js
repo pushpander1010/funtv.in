c9360341f615daf152e9ab879412b23b3403de30
pushpander1010 <2023mt93337@wilp.bits-pilani.ac.in>
2026-01-03 18:35:12 +0530

    Massive fault tolerance improvements: 87 streaming sources, retry logic, emergency backups, health monitoring

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use('/app.js', (req, res, next) => {
 
 app.use(express.static('public'));
 
-// Comprehensive streaming sources for maximum availability
+// Comprehensive streaming sources for maximum availability and fault tolerance
 const STREAMING_SOURCES = [
-  // Primary IPTV Sources
+  // Primary IPTV Sources (Highly Reliable)
   {
     name: 'IPTV-org Main',
     url: 'https://iptv-org.github.io/iptv/index.m3u',
@@ -56,75 +56,522 @@ const STREAMING_SOURCES = [
     priority: 4
   },
   {
-    name: 'Free-TV',
-    url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
+    name: 'IPTV-org Entertainment',
+    url: 'https://iptv-org.github.io/iptv/categories/entertainment.m3u',
     type: 'iptv',
     priority: 5
   },
-  // Alternative IPTV Sources
   {
-    name: 'IPTV Collection',
-    url: 'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/us.m3u',
+    name: 'IPTV-org Movies',
+    url: 'https://iptv-org.github.io/iptv/categories/movies.m3u',
     type: 'iptv',
     priority: 6
   },
+
+  // Alternative IPTV Sources (Backup Sources)
   {
-    name: 'World IPTV',
-    url: 'https://raw.githubusercontent.com/hosseinpourziyaie/IPTV-WORLD/main/IPTV-WORLD.m3u',
+    name: 'Free-TV Main',
+    url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
     type: 'iptv',
     priority: 7
   },
   {
-    name: 'Global IPTV',
-    url: 'https://raw.githubusercontent.com/davidmuma/Canales_dobleM/master/TDT_ES/playlist.m3u8',
+    name: 'IPTV-org UK',
+    url: 'https://iptv-org.github.io/iptv/countries/uk.m3u',
     type: 'iptv',
     priority: 8
   },
-  // Web TV Sources
   {
-    name: 'WebTV Collection',
-    url: 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/youtube.m3u',
-    type: 'webtv',
+    name: 'IPTV-org IN',
+    url: 'https://iptv-org.github.io/iptv/countries/in.m3u',
+    type: 'iptv',
     priority: 9
   },
   {
-    name: 'YouTube Live',
-    url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlists/playlist_youtube.m3u8',
-    type: 'webtv',
+    name: 'IPTV-org CA',
+    url: 'https://iptv-org.github.io/iptv/countries/ca.m3u',
+    type: 'iptv',
     priority: 10
   },
-  // Radio Sources
+  {
+    name: 'IPTV-org AU',
+    url: 'https://iptv-org.github.io/iptv/countries/au.m3u',
+    type: 'iptv',
+    priority: 11
+  },
+
+  // Additional Reliable Sources
+  {
+    name: 'IPTV-org DE',
+    url: 'https://iptv-org.github.io/iptv/countries/de.m3u',
+    type: 'iptv',
+    priority: 12
+  },
+  {
+    name: 'IPTV-org FR',
+    url: 'https://iptv-org.github.io/iptv/countries/fr.m3u',
+    type: 'iptv',
+    priority: 13
+  },
+  {
+    name: 'IPTV-org IT',
+    url: 'https://iptv-org.github.io/iptv/countries/it.m3u',
+    type: 'iptv',
+    priority: 14
+  },
+  {
+    name: 'IPTV-org ES',
+    url: 'https://iptv-org.github.io/iptv/countries/es.m3u',
+    type: 'iptv',
+    priority: 15
+  },
+  {
+    name: 'IPTV-org BR',
+    url: 'https://iptv-org.github.io/iptv/countries/br.m3u',
+    type: 'iptv',
+    priority: 16
+  },
+
+  // Web TV Sources (YouTube-based)
+  {
+    name: 'YouTube TV Collection',
+    url: 'https://raw.githubusercontent.com/benmoose39/YouTube_to_m3u/main/youtube.m3u',
+    type: 'webtv',
+    priority: 17
+  },
+  {
+    name: 'YouTube Live Sports',
+    url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlists/sports.m3u8',
+    type: 'webtv',
+    priority: 18
+  },
+  {
+    name: 'YouTube News',
+    url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlists/news.m3u8',
+    type: 'webtv',
+    priority: 19
+  },
+
+  // Radio Sources (Highly Reliable)
   {
     name: 'Radio Browser Top',
     url: 'https://de1.api.radio-browser.info/m3u/stations/topvote/200',
     type: 'radio',
-    priority: 11
+    priority: 20
   },
   {
     name: 'Radio Browser Popular',
     url: 'https://de1.api.radio-browser.info/m3u/stations/topclick/200',
     type: 'radio',
-    priority: 12
-  },
-  // Premium Free Services (when available)
-  {
-    name: 'Pluto TV Channels',
-    url: 'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/PlutoTV/all.m3u8',
-    type: 'pluto',
-    priority: 13
+    priority: 21
   },
   {
-    name: 'Tubi Streams',
-    url: 'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/master/Tubi/all.m3u8',
-    type: 'tubi',
-    priority: 14
+    name: 'Radio Browser Recent',
+    url: 'https://de1.api.radio-browser.info/m3u/stations/lastchange/200',
+    type: 'radio',
+    priority: 22
   },
-  // Additional Sources
+
+  // Additional Free Streaming Sources
   {
-    name: 'Awesome IPTV',
-    url: 'https://raw.githubusercontent.com/iptv-org/awesome-iptv/master/README.md',
+    name: 'IPTV-org Music',
+    url: 'https://iptv-org.github.io/iptv/categories/music.m3u',
     type: 'iptv',
-    priority: 15
+    priority: 23
+  },
+  {
+    name: 'IPTV-org Kids',
+    url: 'https://iptv-org.github.io/iptv/categories/kids.m3u',
+    type: 'iptv',
+    priority: 24
+  },
+  {
+    name: 'IPTV-org Documentary',
+    url: 'https://iptv-org.github.io/iptv/categories/documentary.m3u',
+    type: 'iptv',
+    priority: 25
+  },
+  {
+    name: 'IPTV-org Science',
+    url: 'https://iptv-org.github.io/iptv/categories/science.m3u',
+    type: 'iptv',
+    priority: 26
+  },
+  {
+    name: 'IPTV-org Lifestyle',
+    url: 'https://iptv-org.github.io/iptv/categories/lifestyle.m3u',
+    type: 'iptv',
+    priority: 27
+  },
+
+  // Regional Sources for Better Coverage
+  {
+    name: 'IPTV-org AR',
+    url: 'https://iptv-org.github.io/iptv/countries/ar.m3u',
+    type: 'iptv',
+    priority: 28
+  },
+  {
+    name: 'IPTV-org MX',
+    url: 'https://iptv-org.github.io/iptv/countries/mx.m3u',
+    type: 'iptv',
+    priority: 29
+  },
+  {
+    name: 'IPTV-org JP',
+    url: 'https://iptv-org.github.io/iptv/countries/jp.m3u',
+    type: 'iptv',
+    priority: 30
+  },
+  {
+    name: 'IPTV-org KR',
+    url: 'https://iptv-org.github.io/iptv/countries/kr.m3u',
+    type: 'iptv',
+    priority: 31
+  },
+  {
+    name: 'IPTV-org RU',
+    url: 'https://iptv-org.github.io/iptv/countries/ru.m3u',
+    type: 'iptv',
+    priority: 32
+  },
+
+  // Additional Backup Sources
+  {
+    name: 'IPTV-org CN',
+    url: 'https://iptv-org.github.io/iptv/countries/cn.m3u',
+    type: 'iptv',
+    priority: 33
+  },
+  {
+    name: 'IPTV-org TR',
+    url: 'https://iptv-org.github.io/iptv/countries/tr.m3u',
+    type: 'iptv',
+    priority: 34
+  },
+  {
+    name: 'IPTV-org NL',
+    url: 'https://iptv-org.github.io/iptv/countries/nl.m3u',
+    type: 'iptv',
+    priority: 35
+  },
+  {
+    name: 'IPTV-org SE',
+    url: 'https://iptv-org.github.io/iptv/countries/se.m3u',
+    type: 'iptv',
+    priority: 36
+  },
+  {
+    name: 'IPTV-org NO',
+    url: 'https://iptv-org.github.io/iptv/countries/no.m3u',
+    type: 'iptv',
+    priority: 37
+  },
+
+  // More Category Sources
+  {
+    name: 'IPTV-org Auto',
+    url: 'https://iptv-org.github.io/iptv/categories/auto.m3u',
+    type: 'iptv',
+    priority: 38
+  },
+  {
+    name: 'IPTV-org Business',
+    url: 'https://iptv-org.github.io/iptv/categories/business.m3u',
+    type: 'iptv',
+    priority: 39
+  },
+  {
+    name: 'IPTV-org Cooking',
+    url: 'https://iptv-org.github.io/iptv/categories/cooking.m3u',
+    type: 'iptv',
+    priority: 40
+  },
+  {
+    name: 'IPTV-org Education',
+    url: 'https://iptv-org.github.io/iptv/categories/education.m3u',
+    type: 'iptv',
+    priority: 41
+  },
+  {
+    name: 'IPTV-org Family',
+    url: 'https://iptv-org.github.io/iptv/categories/family.m3u',
+    type: 'iptv',
+    priority: 42
+  },
+  {
+    name: 'IPTV-org Fashion',
+    url: 'https://iptv-org.github.io/iptv/categories/fashion.m3u',
+    type: 'iptv',
+    priority: 43
+  },
+  {
+    name: 'IPTV-org Food',
+    url: 'https://iptv-org.github.io/iptv/categories/food.m3u',
+    type: 'iptv',
+    priority: 44
+  },
+  {
+    name: 'IPTV-org Gaming',
+    url: 'https://iptv-org.github.io/iptv/categories/gaming.m3u',
+    type: 'iptv',
+    priority: 45
+  },
+  {
+    name: 'IPTV-org Health',
+    url: 'https://iptv-org.github.io/iptv/categories/health.m3u',
+    type: 'iptv',
+    priority: 46
+  },
+  {
+    name: 'IPTV-org History',
+    url: 'https://iptv-org.github.io/iptv/categories/history.m3u',
+    type: 'iptv',
+    priority: 47
+  },
+  {
+    name: 'IPTV-org Hobby',
+    url: 'https://iptv-org.github.io/iptv/categories/hobby.m3u',
+    type: 'iptv',
+    priority: 48
+  },
+  {
+    name: 'IPTV-org Legislative',
+    url: 'https://iptv-org.github.io/iptv/categories/legislative.m3u',
+    type: 'iptv',
+    priority: 49
+  },
+  {
+    name: 'IPTV-org Local',
+    url: 'https://iptv-org.github.io/iptv/categories/local.m3u',
+    type: 'iptv',
+    priority: 50
+  },
+  {
+    name: 'IPTV-org Nature',
+    url: 'https://iptv-org.github.io/iptv/categories/nature.m3u',
+    type: 'iptv',
+    priority: 51
+  },
+  {
+    name: 'IPTV-org Religious',
+    url: 'https://iptv-org.github.io/iptv/categories/religious.m3u',
+    type: 'iptv',
+    priority: 52
+  },
+  {
+    name: 'IPTV-org Shop',
+    url: 'https://iptv-org.github.io/iptv/categories/shop.m3u',
+    type: 'iptv',
+    priority: 53
+  },
+  {
+    name: 'IPTV-org Travel',
+    url: 'https://iptv-org.github.io/iptv/categories/travel.m3u',
+    type: 'iptv',
+    priority: 54
+  },
+  {
+    name: 'IPTV-org Weather',
+    url: 'https://iptv-org.github.io/iptv/categories/weather.m3u',
+    type: 'iptv',
+    priority: 55
+  },
+
+  // Additional Reliable Sources (High Priority Backups)
+  {
+    name: 'IPTV-org XX',
+    url: 'https://iptv-org.github.io/iptv/countries/xx.m3u',
+    type: 'iptv',
+    priority: 56
+  },
+  {
+    name: 'IPTV-org BE',
+    url: 'https://iptv-org.github.io/iptv/countries/be.m3u',
+    type: 'iptv',
+    priority: 57
+  },
+  {
+    name: 'IPTV-org CH',
+    url: 'https://iptv-org.github.io/iptv/countries/ch.m3u',
+    type: 'iptv',
+    priority: 58
+  },
+  {
+    name: 'IPTV-org DK',
+    url: 'https://iptv-org.github.io/iptv/countries/dk.m3u',
+    type: 'iptv',
+    priority: 59
+  },
+  {
+    name: 'IPTV-org FI',
+    url: 'https://iptv-org.github.io/iptv/countries/fi.m3u',
+    type: 'iptv',
+    priority: 60
+  },
+  {
+    name: 'IPTV-org GR',
+    url: 'https://iptv-org.github.io/iptv/countries/gr.m3u',
+    type: 'iptv',
+    priority: 61
+  },
+  {
+    name: 'IPTV-org HU',
+    url: 'https://iptv-org.github.io/iptv/countries/hu.m3u',
+    type: 'iptv',
+    priority: 62
+  },
+  {
+    name: 'IPTV-org IE',
+    url: 'https://iptv-org.github.io/iptv/countries/ie.m3u',
+    type: 'iptv',
+    priority: 63
+  },
+  {
+    name: 'IPTV-org IL',
+    url: 'https://iptv-org.github.io/iptv/countries/il.m3u',
+    type: 'iptv',
+    priority: 64
+  },
+  {
+    name: 'IPTV-org MY',
+    url: 'https://iptv-org.github.io/iptv/countries/my.m3u',
+    type: 'iptv',
+    priority: 65
+  },
+  {
+    name: 'IPTV-org NZ',
+    url: 'https://iptv-org.github.io/iptv/countries/nz.m3u',
+    type: 'iptv',
+    priority: 66
+  },
+  {
+    name: 'IPTV-org PH',
+    url: 'https://iptv-org.github.io/iptv/countries/ph.m3u',
+    type: 'iptv',
+    priority: 67
+  },
+  {
+    name: 'IPTV-org PL',
+    url: 'https://iptv-org.github.io/iptv/countries/pl.m3u',
+    type: 'iptv',
+    priority: 68
+  },
+  {
+    name: 'IPTV-org PT',
+    url: 'https://iptv-org.github.io/iptv/countries/pt.m3u',
+    type: 'iptv',
+    priority: 69
+  },
+  {
+    name: 'IPTV-org RO',
+    url: 'https://iptv-org.github.io/iptv/countries/ro.m3u',
+    type: 'iptv',
+    priority: 70
+  },
+  {
+    name: 'IPTV-org RS',
+    url: 'https://iptv-org.github.io/iptv/countries/rs.m3u',
+    type: 'iptv',
+    priority: 71
+  },
+  {
+    name: 'IPTV-org SG',
+    url: 'https://iptv-org.github.io/iptv/countries/sg.m3u',
+    type: 'iptv',
+    priority: 72
+  },
+  {
+    name: 'IPTV-org TH',
+    url: 'https://iptv-org.github.io/iptv/countries/th.m3u',
+    type: 'iptv',
+    priority: 73
+  },
+  {
+    name: 'IPTV-org TW',
+    url: 'https://iptv-org.github.io/iptv/countries/tw.m3u',
+    type: 'iptv',
+    priority: 74
+  },
+  {
+    name: 'IPTV-org UA',
+    url: 'https://iptv-org.github.io/iptv/countries/ua.m3u',
+    type: 'iptv',
+    priority: 75
+  },
+  {
+    name: 'IPTV-org VN',
+    url: 'https://iptv-org.github.io/iptv/countries/vn.m3u',
+    type: 'iptv',
+    priority: 76
+  },
+  {
+    name: 'IPTV-org ZA',
+    url: 'https://iptv-org.github.io/iptv/countries/za.m3u',
+    type: 'iptv',
+    priority: 77
+  },
+
+  // More Category Sources for Comprehensive Coverage
+  {
+    name: 'IPTV-org Animation',
+    url: 'https://iptv-org.github.io/iptv/categories/animation.m3u',
+    type: 'iptv',
+    priority: 78
+  },
+  {
+    name: 'IPTV-org Comedy',
+    url: 'https://iptv-org.github.io/iptv/categories/comedy.m3u',
+    type: 'iptv',
+    priority: 79
+  },
+  {
+    name: 'IPTV-org Crime',
+    url: 'https://iptv-org.github.io/iptv/categories/crime.m3u',
+    type: 'iptv',
+    priority: 80
+  },
+  {
+    name: 'IPTV-org Drama',
+    url: 'https://iptv-org.github.io/iptv/categories/drama.m3u',
+    type: 'iptv',
+    priority: 81
+  },
+  {
+    name: 'IPTV-org Mystery',
+    url: 'https://iptv-org.github.io/iptv/categories/mystery.m3u',
+    type: 'iptv',
+    priority: 82
+  },
+  {
+    name: 'IPTV-org Romance',
+    url: 'https://iptv-org.github.io/iptv/categories/romance.m3u',
+    type: 'iptv',
+    priority: 83
+  },
+  {
+    name: 'IPTV-org Sci-Fi',
+    url: 'https://iptv-org.github.io/iptv/categories/sci-fi.m3u',
+    type: 'iptv',
+    priority: 84
+  },
+  {
+    name: 'IPTV-org Thriller',
+    url: 'https://iptv-org.github.io/iptv/categories/thriller.m3u',
+    type: 'iptv',
+    priority: 85
+  },
+  {
+    name: 'IPTV-org War',
+    url: 'https://iptv-org.github.io/iptv/categories/war.m3u',
+    type: 'iptv',
+    priority: 86
+  },
+  {
+    name: 'IPTV-org Western',
+    url: 'https://iptv-org.github.io/iptv/categories/western.m3u',
+    type: 'iptv',
+    priority: 87
   }
 ];
 
@@ -192,34 +639,83 @@ async function loadChannelsFromSources() {
   sourceStats = {};
   channelAlternatives.clear();
 
-  for (const source of STREAMING_SOURCES) {
-    try {
-      console.log(`Fetching ${source.type.toUpperCase()} from ${source.name}...`);
-      const response = await axios.get(source.url, { 
-        timeout: 25000, // Increased timeout for more sources
-        headers: {
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
+  // Function to fetch with retry logic
+  async function fetchWithRetry(source, maxRetries = 3) {
+    for (let attempt = 1; attempt <= maxRetries; attempt++) {
+      try {
+        console.log(`Fetching ${source.type.toUpperCase()} from ${source.name}... (attempt ${attempt}/${maxRetries})`);
+        const response = await axios.get(source.url, {
+          timeout: 30000, // Increased timeout
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'Accept': 'text/plain, */*; q=0.01',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'no-cache'
+          }
+        });
+
+        const sourceChannels = parseM3U(response.data, source.name, source.type);
+        allChannels.push(...sourceChannels);
+
+        sourceStats[source.name] = {
+          channels: sourceChannels.length,
+          status: 'success',
+          type: source.type,
+          attempts: attempt
+        };
+
+        console.log(`✓ ${source.name} (${source.type}): ${sourceChannels.length} channels`);
+        return true; // Success
+      } catch (error) {
+        console.warn(`✗ ${source.name} attempt ${attempt}/${maxRetries}: ${error.message}`);
+
+        if (attempt === maxRetries) {
+          sourceStats[source.name] = {
+            channels: 0,
+            status: 'failed',
+            error: error.message,
+            type: source.type,
+            attempts: attempt
+          };
+          return false; // Failed after all retries
         }
-      });
-      
-      const sourceChannels = parseM3U(response.data, source.name, source.type);
-      allChannels.push(...sourceChannels);
-      
-      sourceStats[source.name] = {
-        channels: sourceChannels.length,
-        status: 'success',
-        type: source.type
-      };
-      
-      console.log(`✓ ${source.name} (${source.type}): ${sourceChannels.length} channels`);
-    } catch (error) {
-      console.error(`✗ ${source.name}: ${error.message}`);
-      sourceStats[source.name] = {
-        channels: 0,
-        status: 'failed',
-        error: error.message,
-        type: source.type
-      };
+
+        // Wait before retry (exponential backoff)
+        await new Promise(resolve => setTimeout(resolve, Math.pow(2, attempt) * 1000));
+      }
+    }
+  }
+
+  // Process sources in parallel with concurrency control
+  const concurrencyLimit = 5; // Process 5 sources at a time
+  for (let i = 0; i < STREAMING_SOURCES.length; i += concurrencyLimit) {
+    const batch = STREAMING_SOURCES.slice(i, i + concurrencyLimit);
+    await Promise.all(batch.map(source => fetchWithRetry(source)));
+  }
+
+  // If we have very few channels, try some emergency backup sources
+  if (allChannels.length < 1000) {
+    console.log('Low channel count detected, trying emergency backup sources...');
+
+    const emergencySources = [
+      {
+        name: 'Emergency IPTV-org All',
+        url: 'https://iptv-org.github.io/iptv/index.m3u',
+        type: 'iptv',
+        priority: 999
+      },
+      {
+        name: 'Emergency Free-TV Backup',
+        url: 'https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8',
+        type: 'iptv',
+        priority: 1000
+      }
+    ];
+
+    for (const source of emergencySources) {
+      if (!sourceStats[source.name]) {
+        await fetchWithRetry(source, 2); // Only 2 retries for emergency sources
+      }
     }
   }
 
@@ -486,6 +982,31 @@ app.get('/api/categories', (req, res) => {
     
   const categories = [...new Set(sourceChannels.map(ch => ch.category))].sort();
   res.json(categories);
+});
+
+// Health check endpoint
+app.get('/api/health', (req, res) => {
+  const totalSources = STREAMING_SOURCES.length;
+  const failedSources = Object.values(sourceStats).filter(stat => stat.status === 'failed').length;
+  const successfulSources = Object.values(sourceStats).filter(stat => stat.status === 'success').length;
+
+  res.json({
+    status: 'healthy',
+    timestamp: new Date().toISOString(),
+    channels: {
+      total: channels.length,
+      validated: validatedChannels.length,
+      validationInProgress
+    },
+    sources: {
+      total: totalSources,
+      successful: successfulSources,
+      failed: failedSources,
+      successRate: totalSources > 0 ? Math.round((successfulSources / totalSources) * 100) : 0
+    },
+    uptime: process.uptime(),
+    memory: process.memoryUsage()
+  });
 });
 
 app.get('/api/validation-status', (req, res) => {
